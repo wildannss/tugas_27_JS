7cefe5afb5a1aec001ef730c7f81d68324cb8277
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:33:00 +0700

    tugas niomic

---
+++ tugas27.js
@@ -0,0 +1,8 @@
+function tugas27(){
+    let x = ["No", "rumah saya", 30, "A"];
+    let y = ["Jalan pangeran Tirtayasa No", 70];
+    console.log(x.join(" "));
+    console.log(x.join(" "));
+};
+
+tugas27();
